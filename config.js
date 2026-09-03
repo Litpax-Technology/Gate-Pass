96f32a34cfbf13ce12eb27db5e8f51e6d3c84551
Litpax-Technology <mis@litpaxtechnology.com>
2026-09-03 10:13:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /* Gate Pass System — configuration
    Paste your deployed Apps Script /exec URL below. */
 const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbyd-aUFbxlsqj1A3egOEhkDoCd25AhpWZefHYMqqbLg51qi3LwkU9QvzU25YxVa5x7x/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzvoAj1-J6-_KBek9u8G3CC45diqKvRCahObTkgkjUEBJk00OWU3f66ISdM_ghgnJlV/exec',
   APP_NAME: 'Gate Pass System',
   PAGE_SIZE: 50
 };
